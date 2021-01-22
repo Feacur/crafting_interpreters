1f355e8a5ff8bf4fc54c1de88f831881bdb988c3
feacur <feacur@gmail.com>
2021-01-22 22:46:05 +0300

    finished chapter "Scanning"

---
+++ code/Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+class Lox
+{
+	private static bool hadError;
+
+	static void Main(string[] args)
+	{
+		if (args.Length == 0) {
+			RunPrompt();
+			return;
+		}
+
+		if (args.Length > 1) {
+			Console.WriteLine("usage: interpreter [script]");
+			Environment.Exit(1); return;
+		}
+
+		RunFile(args[0]);
+	}
+
+	private static void RunFile(string path)
+	{
+		string source = File.ReadAllText(path, Encoding.Default);
+		RunSource(source);
+		if (hadError) { Environment.Exit(2); return; }
+	}
+
+	private static void RunPrompt()
+	{
+		for (;;) {
+			Console.Write("> ");
+			string line = Console.ReadLine();
+			if (line == null) { break; }
+			RunSource(line);
+			hadError = false;
+		}
+	}
+
+	private static void RunSource(string source)
+	{
+		if (string.IsNullOrEmpty(source)) { return; }
+		Scanner scanner = new Scanner(source);
+		List<Token> tokens = scanner.ScanTokens();
+
+		foreach (Token token in tokens)
+		{
+			Console.WriteLine(token);
+		}
+	}
+
+	public static void Error(int line, string message) => Report(line, string.Empty, message);
+
+	public static void Report(int line, string where, string message)
+	{
+		System.Console.WriteLine("[line " + line + "] Error" + where + ": " + message);
+		hadError = true;
+	}
+}
---
+++ code/Scanner.cs
@@ -0,0 +1,180 @@
+using System.Collections.Generic;
+
+public class Scanner
+{
+	private string source;
+	private List<Token> tokens = new List<Token>();
+	private int start = 0;
+	private int current = 0;
+	private int line = 0;
+
+	public Scanner(string source)
+	{
+		this.source = source;
+	}
+
+	public List<Token> ScanTokens()
+	{
+		while (!IsAtEnd()) {
+			start = current;
+			ScanToken();
+		}
+
+		tokens.Add(new Token(TokenType.EOF, string.Empty, null, line));
+		return tokens;
+	}
+
+	private void ScanToken()
+	{
+		char c = Advance();
+		switch (c) {
+			//
+			case '(': AddToken(TokenType.LEFT_PAREN); break;
+			case ')': AddToken(TokenType.RIGHT_PAREN); break;
+			case '{': AddToken(TokenType.LEFT_BRACE); break;
+			case '}': AddToken(TokenType.RIGHT_BRACE); break;
+			case ',': AddToken(TokenType.COMMA); break;
+			case '.': AddToken(TokenType.DOT); break;
+			case '-': AddToken(TokenType.MINUS); break;
+			case '+': AddToken(TokenType.PLUS); break;
+			case ';': AddToken(TokenType.SEMICOLON); break;
+			case '*': AddToken(TokenType.STAR); break;
+
+			//
+			case '!': AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+			case '=': AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+			case '<': AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+			case '>': AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+
+			//
+			case '/': {
+				if (Match('/')) {
+					while (Peek() != '\n' && !IsAtEnd()) { Advance(); }
+				}
+				else {
+					AddToken(TokenType.SLASH);
+				}
+			} break;
+
+			//
+			case ' ':
+			case '\t':
+			case '\r':
+				break;
+
+			case '\n': line++; break;
+
+			//
+			case '"': AddString(); break;
+
+			default: {
+				if (IsDigit(c)) { AddNumber(); break; }
+				if (IsAlpha(c)) { AddIdentifier(); break; }
+				Lox.Error(line, "unexpected character");
+			} break;
+		}
+	}
+
+	private bool Match(char expected)
+	{
+		if (IsAtEnd()) { return false; }
+		if (source[current] != expected) { return false; }
+		current++; return true;
+	}
+
+	private char Peek()
+	{
+		if (IsAtEnd()) { return '\0'; }
+		return source[current];
+	}
+
+	private char PeekNext()
+	{
+		if (current + 1 >= source.Length) { return '\0'; }
+		return source[current + 1];
+	}
+
+	private char Advance()
+	{
+		return source[current++];
+	}
+
+	private void AddString()
+	{
+		while (Peek() != '"' && !IsAtEnd()) {
+			if (Peek() == '\n') { line++; }
+			Advance();
+		}
+
+		if (IsAtEnd()) {
+			Lox.Error(line, "unterminated string");
+			return;
+		}
+
+		Advance(); // consume closing quote
+
+		string lexeme = source.Substring(start + 1, current - start - 2);
+		AddToken(TokenType.STRING, lexeme);
+	}
+
+	private void AddNumber()
+	{
+		while (IsDigit(Peek())) { Advance(); }
+
+		if (Peek() == '.' && IsDigit(PeekNext())) {
+			Advance();
+			while (IsDigit(Peek())) { Advance(); }
+		}
+
+		AddToken(TokenType.NUMBER, double.Parse(source.Substring(start, current - start)));
+	}
+
+	private void AddIdentifier()
+	{
+		while (IsAlphaNumeric(Peek())) { Advance(); }
+
+		string text = source.Substring(start, current - start);
+		if (!keywords.TryGetValue(text, out TokenType type)) {
+			type = TokenType.IDENTIFIER;
+		}
+
+		AddToken(type);
+	}
+
+	private void AddToken(TokenType type) => AddToken(type, null);
+
+	private void AddToken(TokenType type, object literal)
+	{
+		string lexeme = source.Substring(start, current - start);
+		tokens.Add(new Token(type, lexeme, literal, line));
+	}
+
+	private bool IsAtEnd() => current >= source.Length;
+
+	private static bool IsDigit(char c) => '0' <= c && c <= '9';
+
+	private static bool IsAlpha(char c) => 'a' <= c && c <= 'z'
+	                                    || 'A' <= c && c <= 'Z'
+	                                    || c == '_';
+
+	private static bool IsAlphaNumeric(char c) => IsAlpha(c) || IsDigit(c);
+
+	private static Dictionary<string, TokenType> keywords = new Dictionary<string, TokenType> {
+		{"and", TokenType.AND},
+		{"class", TokenType.CLASS},
+		{"else", TokenType.ELSE},
+		{"false", TokenType.FALSE},
+		{"for", TokenType.FOR},
+		{"fun", TokenType.FUN},
+		{"if", TokenType.IF},
+		{"nil", TokenType.NIL},
+		{"or", TokenType.OR},
+		{"print", TokenType.PRINT},
+		{"return", TokenType.RETURN},
+		{"super", TokenType.SUPER},
+		{"this", TokenType.THIS},
+		{"true", TokenType.TRUE},
+		{"var", TokenType.VAR},
+		{"while", TokenType.WHILE},
+	};
+}
