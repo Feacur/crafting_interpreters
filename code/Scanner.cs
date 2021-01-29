317b95d1800f8615f18f8e1f41b0f5bcaf62976e
feacur <feacur@gmail.com>
2021-01-30 00:26:31 +0300

    ok, btw, fix the line number

--- code/Scanner.cs
+++ code/Scanner.cs
@@ -7,7 +7,7 @@ public class Scanner
 	private readonly List<Token> tokens = new List<Token>();
 	private int start = 0;
 	private int current = 0;
-	private int line = 0;
+	private int line = 1;
 
 	public Scanner(string source)
 	{
